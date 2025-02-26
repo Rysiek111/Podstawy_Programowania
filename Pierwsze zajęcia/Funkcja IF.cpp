e357b5614324c064907d742058fac6f51c25e71b
Rysiek111 <95545063+Rysiek111@users.noreply.github.com>
2025-02-26 21:21:32 +0100

    Update Funkcja IF.cpp

--- Pierwsze zajęcia/Funkcja IF.cpp
+++ Pierwsze zajęcia/Funkcja IF.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main() {
     int x;
-    cout<<"Podaj wartosc x: ";
+    cout << "Podaj wartosc x: ";
     cin>>x;
 
     if(x>0)
-       cout<<"Podales wartosc dodatnia"<<endl;
+       cout << "Podales wartosc dodatnia" << endl;
     else
-       cout<<"Podales wartosc niedodatnia"<<endl;
+       cout << "Podales wartosc niedodatnia" << endl;
 
     return 0;
 }
