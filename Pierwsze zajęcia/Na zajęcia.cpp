66b56dd204c0bdbab9b3c2c329813e2d65f4d1dc
Rysiek111 <95545063+Rysiek111@users.noreply.github.com>
2025-03-03 20:35:31 +0100

    Update Na zajęcia.cpp

---
+++ Pierwsze zajęcia/Na zajęcia.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x;
+    cout<<"Podaj wartosc x: ";
+    cin>>x;
+
+    if(x>0)
+       cout<<"Podales wartosc dodatnia"<<endl;
+
+    else
+       if(X==0)
+       cout<<"Podales wartosc zerowa"<<endl;
+
+       else
+       cout<<"Podales wartosc ujemna"<<endl;
+
+    return 0;
+}
