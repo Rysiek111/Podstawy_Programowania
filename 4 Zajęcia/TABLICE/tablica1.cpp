75164961b80ca954daf1ae577055378d4da9de78
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-17 15:21:21 +0100

    Create tablica1.cpp

---
+++ 4 Zajęcia/TABLICE/tablica1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std;
+
+main()
+{
+int w[10];
+
+for(int i=0;i<=9;i++){
+cout<<"w["<<i<<"]= ";
+cin>>w[i];
+}
+for(int i=0;i<=9;i++)
+cout<<"w["<<i<<"]= "<<w[i]<<", ";
+
+}
