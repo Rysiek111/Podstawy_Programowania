e56c7adc9cd7b0d1e39b92178637c377c4fdc26d
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-17 23:39:09 +0100

    Update Zadanie 1.cpp

--- Czwarte Zajęcia/Zadanie 1.cpp
+++ Czwarte Zajęcia/Zadanie 1.cpp
@@ -1,22 +1,22 @@
-//Wypisz wszystkie pary liczb z zaktesu od 1 do n. Wartosc n podaje uzytkownik.
+// Wypisz wszystkie pary liczb z zakresu od 1 do n. Wartosc n podaje użytkownik.
 
-#include <iostream>
+#include <iostream> // Biblioteka do obsługi wejścia i wyjścia
 
-using namespace std;
+using namespace std; // Używanie przestrzeni nazw std, aby uniknąć pisania std:: przed cout i cin
 
 int main()
 {
-    int n, suma;
-    double iloczyn;
+    int n; // Zmienna do przechowywania liczby n, podanej przez użytkownika
 
-    cout<<"Podaj ile razy mam się powtorzyc: ";
-    cin>>n;
+    // Prośba o podanie liczby n (zakresu)
+    cout << "Podaj ile razy mam się powtorzyc: "; 
+    cin >> n; // Wczytanie wartości n od użytkownika
 
-    suma=0;
-    iloczyn=1;
-    for(int i=1; i<=n; i++)
-        for(int j=1; j<=n; j++)
-            cout<<"i= "<<i<<", j="<<j<<endl;
+    // Podwójna pętla for generująca wszystkie możliwe pary (i, j) dla 1 ≤ i, j ≤ n
+    for (int i = 1; i <= n; i++) // Pętla zewnętrzna dla wartości i
+        for (int j = 1; j <= n; j++) // Pętla wewnętrzna dla wartości j
+            // Wypisanie pary liczb i i j
+            cout << "i = " << i << ", j = " << j << endl;
 
-    return 0;
+    return 0; // Zakończenie programu
 }
