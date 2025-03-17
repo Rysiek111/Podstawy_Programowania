6957d286e6846abcf984cb76dff7d082a3550938
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-17 23:30:41 +0100

    Update Zadanie 4.cpp

--- Czwarte Zajęcia/Zadanie 4.cpp
+++ Czwarte Zajęcia/Zadanie 4.cpp
@@ -1,10 +1,13 @@
-#include <iostream>
-#include <math.h>
-using namespace std;
+#include <iostream>  // Biblioteka do obsługi wejścia i wyjścia (cin, cout)
+#include <math.h>    // Biblioteka matematyczna (dla funkcji trygonometrycznych, np. sin())
+using namespace std; // Używanie przestrzeni nazw std, aby nie pisać std::cout, std::sin itp.
 
-int main()
+int main() // Główna funkcja programu, zwracająca wartość całkowitą (int)
 {
-    for(double x=0; x<=3.140; x+=0.0001)
-        cout <<"x"<<x<<"\tsin(x)"<<sin(x)<<endl;
-    return 0;
+    // Pętla for iterująca przez wartości x od 0 do 3.140 z krokiem 0.0001
+    for(double x = 0; x <= 3.140; x += 0.0001)
+        // Wypisanie wartości x oraz sin(x) na standardowe wyjście
+        cout << "x = " << x << "\t sin(x) = " << sin(x) << endl;
+
+    return 0; // Zakończenie programu i zwrócenie 0, co oznacza poprawne wykonanie
 }
