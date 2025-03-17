8f1a91ba9cb9ea16331c6100b63f1f468ddc3a27
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-17 13:59:42 +0100

    Add files via upload

---
+++ RG/A2/main.cpp
@@ -0,0 +1,23 @@
+//Wypisz wszystkie pary liczb z zaktesu od 1 do n. Wartosc n podaje uzytkownik.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, suma;
+    double iloczyn;
+
+    cout<<"Podaj ile razy mam się powtorzyc: ";
+    cin>>n;
+
+    suma=0;
+    iloczyn=1;
+    for(int i=1; i<=n; i++)
+        for(int j=1; j<=n; j++){
+            if(i!=j)
+            cout<<"i= "<<i<<", j="<<j<<endl;
+            }
+    return 0;
+}
