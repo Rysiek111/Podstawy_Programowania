f52bf458cfbec318bbbe220d750915f08655bbbe
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-17 15:00:47 +0100

    Create do3(ZadanieDomowe).cpp

---
+++ 4 Zajęcia/DO/do3(ZadanieDomowe).cpp
@@ -0,0 +1,6 @@
+/*
+Zmodyfikować napisany wcześniej dowolny program obliczeniowy (np. liczący pierwiastki, równania kwadratowe) tak,
+żeby na życzenie użytkownika komputer ponawiał obliczenia lub kończył program.
+Do tego celu zastosuj pętlę do while.
+Program powinien uwzględnić przypadkowe naciścięcie klawisza Caps Lock.
+*/
