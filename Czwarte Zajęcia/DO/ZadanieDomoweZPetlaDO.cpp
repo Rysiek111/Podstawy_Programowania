18e65e03bd5e02ee3a089b08bc9bdb820b93ea29
Rysiek111 <95545063+Rysiek111@users.noreply.github.com>
2025-03-17 22:21:49 +0100

    Update ZadanieDomoweZPetlaDO.cpp

--- Czwarte Zajęcia/DO/ZadanieDomoweZPetlaDO.cpp
+++ Czwarte Zajęcia/DO/ZadanieDomoweZPetlaDO.cpp
@@ -7,31 +7,37 @@ Program powinien uwzględnić przypadkowe naciścięcie klawisza Caps Lock.
 
 #include <iostream>
 
-
 using namespace std;
 
-main()
+int main()
 {
     char dec;
-
     cout << "Ten program obliczy pole i obwod prostokata" << endl;
 
     do
     {
         double x, y;
 
-
+        // Pobranie długości boków
         cout << "Podaj dlugosc pierwszego boku: ";
         cin >> x;
         cout << "Podaj dlugosc drugiego boku: ";
         cin >> y;
 
+        // Obliczenia
         cout << "Pole wynosi: " << x * y << endl;
         cout << "Obwod wynosi: " << 2 * (x + y) << endl;
-        cout<<"Czy chcesz powtorzyc ? (T/N)"<<endl;
-        cin>>dec;
+
+        // Zapytanie o ponowne wykonanie programu
+        do
+        {
+            cout << "Czy chcesz powtorzyc? (T/N): ";
+            cin >> dec;
+        }
+        while (dec != 'T' && dec != 't' && dec != 'N' && dec != 'n');
+
     }
-    while((dec!='N')&&(dec!='n'));
+    while (dec == 'T' || dec == 't');
 
+    return 0;
 }
-
