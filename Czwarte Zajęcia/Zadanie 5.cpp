5262eeb5e46c4daf865717868a7e25312510aeaf
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-17 23:28:00 +0100

    Update Zadanie 5.cpp

--- Czwarte Zajęcia/Zadanie 5.cpp
+++ Czwarte Zajęcia/Zadanie 5.cpp
@@ -1,19 +1,22 @@
-#include <iostream>
-#include <math.h>
-using namespace std;
+#include <iostream>  // Biblioteka do obsługi wejścia i wyjścia
+#include <math.h>    // Biblioteka matematyczna
+using namespace std; // Używanie przestrzeni nazw std, aby uniknąć pisania std:: przed cout/cin
 
-main()
+int main()  // Poprawiony nagłówek funkcji main() (brakowało 'int')
 {
-    double a;
-    int b;
+    double a; // Podstawa potęgi
+    int b;    // Wykładnik potęgi
 
-    cout<<"Podaj a: ";
-    cin>>a;
-    cout<<"Podaj b: ";
-    cin>>b;
-    double pot=1;
-    for(int i=1; i<=b; i++)
-        pot*=a;
+    cout << "Podaj a: "; // Prośba o podanie wartości a
+    cin >> a;            // Wczytanie wartości a od użytkownika
+    cout << "Podaj b: "; // Prośba o podanie wartości b
+    cin >> b;            // Wczytanie wartości b od użytkownika
 
-    cout<<"Wartosc wynosi: "<<pot;
+    double pot = 1; // Zmienna przechowująca wynik potęgowania, początkowa wartość 1
+    for(int i = 1; i <= b; i++) // Pętla wykonująca mnożenie a przez siebie b razy
+        pot *= a;               // Mnożenie aktualnej wartości przez a
+
+    cout << "Wartosc wynosi: " << pot; // Wyświetlenie wyniku potęgowania
+
+    return 0; // Zakończenie programu, zwrócenie 0 oznacza poprawne wykonanie
 }
