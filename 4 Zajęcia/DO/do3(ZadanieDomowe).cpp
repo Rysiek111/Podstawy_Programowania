020ee1ac08fc7625c9208bad3051430a2c3364f4
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-17 15:15:52 +0100

    Update do3(ZadanieDomowe).cpp

--- 4 Zajęcia/DO/do3(ZadanieDomowe).cpp
+++ 4 Zajęcia/DO/do3(ZadanieDomowe).cpp
@@ -4,3 +4,34 @@ Zmodyfikować napisany wcześniej dowolny program obliczeniowy (np. liczący pie
 Do tego celu zastosuj pętlę do while.
 Program powinien uwzględnić przypadkowe naciścięcie klawisza Caps Lock.
 */
+
+#include <iostream>
+
+
+using namespace std;
+
+main()
+{
+    char dec;
+
+    cout << "Ten program obliczy pole i obwod prostokata" << endl;
+
+    do
+    {
+        double x, y;
+
+
+        cout << "Podaj dlugosc pierwszego boku: ";
+        cin >> x;
+        cout << "Podaj dlugosc drugiego boku: ";
+        cin >> y;
+
+        cout << "Pole wynosi: " << x * y << endl;
+        cout << "Obwod wynosi: " << 2 * (x + y) << endl;
+        cout<<"Czy chcesz powtorzyc ? (T/N)"<<endl;
+        cin>>dec;
+    }
+    while((dec!='N')&&(dec!='n'));
+
+}
+
