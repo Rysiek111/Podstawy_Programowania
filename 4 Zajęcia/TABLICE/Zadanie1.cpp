1df5fd52f7fff6b68179dab9a5b562d354ac6fe3
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-17 15:39:25 +0100

    Create Zadanie1.cpp

---
+++ 4 Zajęcia/TABLICE/Zadanie1.cpp
@@ -0,0 +1,32 @@
+/*
+Dana jest tablica jedonwymiarowa 10-elementowa o wartościach typu rzeczywistego.
+Napisz program obliczający sumę wartości elementów tablicy.
+Wartości tablicy zostały podane uprzednio przez użytkownika.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int n=10;
+    double w[n];
+
+    for(int i=0; i<=n-1; i++)
+    {
+        cout<<"w["<<i<<"]= ";
+        cin>>w[i];
+    }
+/////////////////////////////////////////
+
+    double suma;
+    for(int x=0; x<=n-1; x++)
+        suma+=w[x];
+    cout<<"Suma: "<<suma<<endl;
+
+/////////////////////////////////////////
+    for(int i=0; i<=n-1; i++)
+        cout<<"w["<<i<<"]= "<<w[i]<<", ";
+
+    return 0;
+}
