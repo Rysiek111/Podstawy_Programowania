dce651ece104d3df7ad60343cd103bce58e9b66b
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-24 15:19:56 +0100

    Update B5(Tablica Dwuwymiarowa).cpp

--- Piate Zajęcia/B5(Tablica Dwuwymiarowa).cpp
+++ Piate Zajęcia/B5(Tablica Dwuwymiarowa).cpp
@@ -9,19 +9,20 @@ int main()
     double a[n][m];
     int i,j;
 
-    for(i=0;i<=n-1;i++)
-        for(j=0;j<=m-1;j++){
+    for(i=0; i<=n-1; i++)
+        for(j=0; j<=m-1; j++)
+        {
             cout<<"a["<<i<<"]["<<j<<"]=";
             cin>>a[i][j];
-            }
+        }
     cout<<endl;
 
-    for(i=0;i<=n-1;i++){
-        for(j=0;j<=m-1;j++){
+    for(i=0; i<=n-1; i++)
+    {
+        for(j=0; j<=m-1; j++)
             cout<<a[i][j]<<"\t";
-            }
         cout<<endl;
-        }
+    }
 
     return 0;
 }
