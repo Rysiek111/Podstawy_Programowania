98f1ac9593219fbd11a6d62df070cd0540c14415
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-24 14:07:03 +0100

    Create B2.cpp

---
+++ Piate Zajęcia/B2.cpp
@@ -0,0 +1,35 @@
+/*
+Dana jest tablica jedonwymiarowa 10-elementowa o wartościach typu rzeczywistego.
+Napisz program automatycznie generujący wartości w tablicy tak,
+zeby w wierszach parzystych zostały przypisane wartości równe pierwiastkowi numeru wiersza,
+a nieparzyste wartości równe 1.
+*/
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    const int n=10;
+    double w[n];
+
+    for(int i=0; i<=n-1; i++)
+    {
+        if(i%2==0)
+            w[i]=sqrt(i);
+        else
+            w[i]=1;
+    }
+
+/////////////////////////////////////////
+
+
+
+/////////////////////////////////////////
+
+    for(int i=0; i<=n-1; i++)
+       cout<<"w["<<i<<"]= "<<w[i]<<", "<<endl;
+
+    return 0;
+}
