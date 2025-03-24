26c094b3e763f6f9afb1eb94943731953ca470aa
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-24 15:17:37 +0100

    Create B5(Tablica Dwuwymiarowa).cpp

---
+++ Piate Zajęcia/B5(Tablica Dwuwymiarowa).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    const int n=10,m=n;
+    double a[n][m];
+    int i,j;
+
+    for(i=0;i<=n-1;i++)
+        for(j=0;j<=m-1;j++){
+            cout<<"a["<<i<<"]["<<j<<"]=";
+            cin>>a[i][j];
+            }
+    cout<<endl;
+
+    for(i=0;i<=n-1;i++){
+        for(j=0;j<=m-1;j++){
+            cout<<a[i][j]<<"\t";
+            }
+        cout<<endl;
+        }
+
+    return 0;
+}
