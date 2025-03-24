c1a945eb33812352bc5c54a7e846f82d30583856
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-24 15:43:43 +0100

    Create B6.cpp

---
+++ Piate Zajęcia/B6.cpp
@@ -0,0 +1,44 @@
+/*
+Dana jest tablica 5x5 o wartościach typu rzeczywistego.
+W pierwszej kolejności dane do tablicy podaje użytkownik.
+Napisz frafment programu obliczający ślad macierzy.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int n=3,m=n;
+    double a[n][m];
+    int i,j;
+
+    for(i=0; i<=n-1; i++)
+        for(j=0; j<=m-1; j++)
+        {
+            cout<<"a["<<i<<"]["<<j<<"]=";
+            cin>>a[i][j];
+        }
+    cout<<endl;
+////////////////////////////////////////////////////////////
+
+    double slad=0;
+
+    for(i=0; i<=n-1; i++)
+    {
+        slad+=a[i][i];
+        cout<<endl;
+    }
+    cout<<"Slad = "<<slad<<endl;
+    cout<<endl;
+
+////////////////////////////////////////////////////////////
+    for(i=0; i<=n-1; i++)
+    {
+        for(j=0; j<=m-1; j++)
+            cout<<a[i][j]<<"\t";
+        cout<<endl;
+    }
+
+    return 0;
+}
