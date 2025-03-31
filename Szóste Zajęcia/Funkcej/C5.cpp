883145f683b627203097807297fe6da57cc1b37d
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-31 15:11:59 +0200

    Create C5.cpp

---
+++ Szóste Zajęcia/Funkcej/C5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void wizytowka(string imie,string nazwisko)
+{
+cout<<"************************"<<endl;
+cout<<"*                      *"<<endl;
+cout<<"*    "<<imie<<"  "<<nazwisko<<"    *"<<endl;
+cout<<"*                      *"<<endl;
+cout<<"************************"<<endl;
+}
+
+int main()
+{
+    wizytowka("Imie","Nazwisko");
+    cout<<"Jakas czesc kodu"<<endl;
+    string im="Imie",na="Nazwisko";
+    wizytowka(im,na);
+    return 0;
+}
