0a35d144bc9252765a9b2e6f6215d8d7c42da45d
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-31 13:59:14 +0200

    Update C.cpp

--- Szóste Zajęcia/C.cpp
+++ Szóste Zajęcia/C.cpp
@@ -22,7 +22,7 @@ int main()
     cout<<endl;
 ////////////////////////////////////////////////////////////
 
-    double ilosc;
+    int ilosc;
     for(i=0; i<=n-1; i++)
         for(j=0; j<=m-1; j++)
         {
