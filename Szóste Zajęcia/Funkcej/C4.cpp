42446d5d89601909cc4f268427aa531247d5a1e2
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-31 14:58:59 +0200

    Create C4.cpp

---
+++ Szóste Zajęcia/Funkcej/C4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void wizytowka()
+{
+cout<<"************************"<<endl;
+cout<<"*                      *"<<endl;
+cout<<"* Początki z Funkcjami *"<<endl;
+cout<<"*                      *"<<endl;
+cout<<"************************"<<endl;
+}
+
+int main()
+{
+    wizytowka();
+    cout<<"Jakas czesc kodu"<<endl;
+    wizytowka();
+    return 0;
+}
