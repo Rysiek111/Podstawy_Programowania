f9ef667cd02e19a7d97efe13644bf0a0539e257b
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-31 15:42:29 +0200

    Create C8.cpp

---
+++ Szóste Zajęcia/Funkcej/C8.cpp
@@ -0,0 +1,5 @@
+/*
+Napisz funkcje max3a i max3b zwracajace maksimum z trzech parametrow rzeczywistych x,y i z:
+-nie wykorzystkiac do tego innych funkcji
+-wykorzystujac do tego celu wczesniej napisana funkcje max2.
+*/
