9bc02818d858c4b7bf7f0d4cb896f4eec7dac640
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-03-31 13:54:46 +0200

    Update C.cpp

--- Szóste Zajęcia/C.cpp
+++ Szóste Zajęcia/C.cpp
@@ -1,1 +1,45 @@
+/*
+Dana jest tablica dwuwymiarowa 4x4 o wartościach typu calkowitego.
+Napisac program zliczajacy liczbe elmentow parzystych w tablicy.
+Wartosci tablicy zostaly podane uprzedniu przez uzytkownika.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int n=2,m=n;
+    int a[n][m];
+    int i,j;
+
+    for(i=0; i<=n-1; i++)
+        for(j=0; j<=m-1; j++)
+        {
+            cout<<"a["<<i<<"]["<<j<<"]=";
+            cin>>a[i][j];
+        }
+    cout<<endl;
+////////////////////////////////////////////////////////////
+
+    double ilosc;
+    for(i=0; i<=n-1; i++)
+        for(j=0; j<=m-1; j++)
+        {
+            if(a[i][j]%2==0)
+                ilosc++;
+        }
+    cout<<"Ilosc: "<<ilosc<<endl;
+    cout<<endl;
+
+////////////////////////////////////////////////////////////
+    for(i=0; i<=n-1; i++)
+    {
+        for(j=0; j<=m-1; j++)
+            cout<<a[i][j]<<"\t";
+        cout<<endl;
+    }
+
+    return 0;
+}
 
