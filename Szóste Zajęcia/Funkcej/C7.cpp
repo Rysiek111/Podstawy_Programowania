7d328d69496e9260537b7aaccf6289d91cebbbc3
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-04-05 18:09:21 +0200

    Update C7.cpp

--- Szóste Zajęcia/Funkcej/C7.cpp
+++ Szóste Zajęcia/Funkcej/C7.cpp
@@ -2,19 +2,18 @@
 Napisac funkcję max2 zwracająca maksimum z dwuch paramertow.
 */
 #include <iostream>
-
 using namespace std;
 
-double max2(double x,double y)
-{
-    if(x>y)
-        return x;
-    else
-        return y;
+double max2(double a, double b) {
+    return (a > b) ? a : b;
 }
 
-int main()
-{
-    cout <<"max2"<< endl;
+int main() {
+    double x, y;
+    cout << "Podaj dwie liczby: ";
+    cin >> x >> y;
+
+    cout << "Maksimum to: " << max2(x, y) << endl;
+
     return 0;
 }
