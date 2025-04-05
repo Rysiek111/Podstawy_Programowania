da6ff67f3685aeebc6c077ba68ce6b52e8b9c541
Rysiek111 <95545063+Rysiek111@users.noreply.github.com>
2025-04-05 20:48:59 +0200

    Siódme Zajęcia

---
+++ 7 Zajęcia/7.2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+double potega(double a, int b)
+{
+double pot=1;
+for(int i=1;i<=b; i++)
+pot*=a;
+return pot;
+}
+double potega2 (double a, int b)
+ {
+if (b==0)
+return 1;
+else
+return a*potega2(a,b-1);
+ }
+int silnia(int a)
+{
+int silnia=1;
+for(int i=2; i<=a; i++)
+silnia*=i;
+return silnia;
+}
+int silnia2(int n)
+{
+if(n==1)
+return 1;
+else
+return n*silnia2(n-1);
+}
+int silnia3(double a, int b)
+{
+if(b==0)
+return 1;
+else
+return a*silnia3(a,b-1);
+}
+int main()
+{
+int n;
+cout<<"Podaj n:"<<endl;
+cin>>n;
+cout<<"silnia2="<<silnia2(n)<<endl;
+cout<<potega(3,3)<<endl;
+cout<<silnia3(5,2)<<endl;
+return 0;
+}
