d2a60984067e543762ad91ac978bc7d3c2fbc177
Ryszard <95545063+Rysiek111@users.noreply.github.com>
2025-05-15 12:46:32 +0200

    Update Przykład z foli 134-133.cpp

--- Jedynaste zajęcia/Przykład z foli 134-133.cpp
+++ Jedynaste zajęcia/Przykład z foli 134-133.cpp
@@ -1,34 +1,39 @@
 #include <iostream>
 using namespace std;
-struct data{
- int dzien,miesiac,rok;
+struct data
+{
+    int dzien,miesiac,rok;
 };
 
-struct osoba{
- char imie[10],nazwisko[20];
- data dataur;
+struct osoba
+{
+    char imie[10],nazwisko[20];
+    data dataur;
 };
 void wpisz(osoba& os)
 {
- cout<<"Podaj dane osobowe:"<<endl;
- cout<<"Imie: "; cin>>os.imie;
- cout<<"Nazwisko: "; cin>>os.nazwisko;
- cout<<"Podaj date urodzenia:"<<endl;
- cout<<" Dzien: "; cin>>os.dataur.dzien;
- cout<<" Miesiac: "; cin>>os.dataur.miesiac;
- cout<<" Rok: "; cin>>os.dataur.rok;
+    cout<<"Podaj dane osobowe:"<<endl;
+    cout<<"Imie: ";
+    cin>>os.imie;
+    cout<<"Nazwisko: ";
+    cin>>os.nazwisko;
+    cout<<"Podaj date urodzenia:"<<endl;
+    cout<<" Dzien: ";
+    cin>>os.dataur.dzien;
+    cout<<" Miesiac: ";
+    cin>>os.dataur.miesiac;
+    cout<<" Rok: ";
+    cin>>os.dataur.rok;
 }
 void wypisz(osoba os)
 {
- cout<<"Imie i Nazwisko: "<<os.imie<<" "<<os.nazwisko<<endl;
- cout<<"Data urodzenia: "<<os.dataur.dzien<<"."
- <<os.dataur.miesiac<<"."
- <<os.dataur.rok<<endl;
+    cout<<"Imie i Nazwisko: "<<os.imie<<" "<<os.nazwisko<<endl;
+    cout<<"Data urodzenia: "<<os.dataur.dzien<<"."<<os.dataur.miesiac<<"."<<os.dataur.rok<<endl;
 }
 main()
 {
- osoba o;
- wpisz(o);
- cout<<endl;
- wypisz(o);
+    osoba o;
+    wpisz(o);
+    cout<<endl;
+    wypisz(o);
 }
